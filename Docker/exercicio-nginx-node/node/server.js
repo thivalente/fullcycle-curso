ef188d4c11212491415ee65d157bd2b7e4d7619d
Thiago Valente <valente.thi@gmail.com>
2024-08-04 21:22:36 -0300

    Exercicio Nginx com node

---
+++ Docker/exercicio-nginx-node/node/server.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const mysql = require('mysql2');
+const Chance = require('chance');
+const app = express();
+const port = 3000;
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config);
+
+// Função para gerar um nome brasileiro aleatório
+const chance = new Chance();
+const firstNames = [
+  'Ana', 'Beatriz', 'Carlos', 'Daniel', 'Eduarda', 'Felipe', 'Gabriela', 'Henrique', 'Isabela', 'João', 'Larissa', 'Marcos', 'Nathalia', 'Otávio', 'Paula', 'Ricardo', 'Sofia', 'Tiago', 'Vitoria', 'Wesley'
+];
+
+const lastNames = [
+  'Silva', 'Santos', 'Oliveira', 'Souza', 'Rodrigues', 'Ferreira', 'Almeida', 'Costa', 'Gomes', 'Martins', 'Araújo', 'Melo', 'Barbosa', 'Ribeiro', 'Alves', 'Pereira', 'Lima', 'Carvalho', 'Teixeira', 'Rocha'
+];
+
+// Função para gerar um nome brasileiro aleatório
+const generateRandomName = () => {
+  const firstName = chance.pickone(firstNames);
+  const lastName = chance.pickone(lastNames);
+  return `${firstName} ${lastName}`;
+};
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Full Cycle Rocks</title>
+        <script>
+          document.addEventListener('DOMContentLoaded', function() {
+            fetch('/get-names')
+              .then(response => response.json())
+              .then(data => {
+                const list = document.getElementById('name-list');
+
+                if (data.length === 0) {
+                  list.innerHTML = '<li>Nenhum nome na lista</li>';
+                } else {
+                  list.innerHTML = data.map(name => \`<li>\${name}</li>\`).join('');
+                }
+
+                const h2 = document.getElementById('h2-subtitle');
+                h2.innerHTML = 'Lista de Nomes';
+              })
+              .catch(error => {
+                console.log(error);
+                document.getElementById('name-list').innerHTML = '<li>Erro ao carregar os nomes</li>';
+              });
+
+            document.getElementById("add").addEventListener("click", function() {
+              fetch('/add-name', { method: 'POST' })
+                .then(response => response.json())
+                .then(data => {
+                  console.log(data.message);
+                  location.reload();
+                })
+                .catch(error => {
+                  console.log(error);
+                  alert('Erro ao adicionar nome');
+                });
+            });
+
+            document.getElementById("clear").addEventListener("click", function() {
+              fetch('/clear-names', { method: 'DELETE' })
+                .then(response => response.json())
+                .then(data => {
+                  console.log(data.message);
+                  location.reload();
+                })
+                .catch(error => {
+                  console.log(error);
+                  alert('Erro ao limpar lista');
+                });
+            });
+          });
+        </script>
+      </head>
+      <body>
+        <h1>Full Cycle Rocks!</h1>
+        <h2 id="h2-subtitle">Carregando a lista de nomes...</h2>
+        <button id="add">Adicionar Novo Nome</button>
+        <button id="clear">Limpar Lista</button>
+        <ul id="name-list"></ul>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/add-name', (req, res) => {
+  const sql = `INSERT INTO people (name) VALUES ('${generateRandomName()}')`;
+
+  connection.query(sql, (error) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'Erro ao adicionar nome' });
+    }
+    res.status(200).json({ message: 'Nome adicionado com sucesso' });
+  });
+});
+
+app.delete('/clear-names', (req, res) => {
+  const sql = 'DELETE FROM people';
+
+  connection.query(sql, (error) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'Erro ao limpar lista' });
+    }
+    res.status(200).json({ message: 'Lista limpa com sucesso' });
+  });
+});
+
+app.get('/get-names', (req, res) => {
+  const query = 'SELECT name FROM people ORDER BY name;';
+
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'Erro ao executar a query' });
+    }
+
+    const names = results.map(person => person.name);
+    res.json(names);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Rodando na porta: ${port}`)
+})
--- README.md
+++ README.md
@@ -15,3 +15,24 @@ Repositório para as atividades do curso Fullcycle 3.0
 **Solução**
 
 Para executar: ``` docker run --rm thivalente/fullcycle ```
+
+### Nginx com Node.js
+
+**Objetivo do exercício**
+
+1. Criar uma imagem docker com Nginx
+2. Criar outra imagem docker com Node.js
+3. Criar outra imagem com banco de dados mysql - deverá existir um banco de dados com uma tabela chamada people (id int, nome varchar 255)
+4. Quando o usuário acessar a aplicação node disponível na porta: 8080, o sistema deverá listar os nomes na tabela e abrir uma página com:
+```
+<h1>Full Cycle Rocks!</h1>
+- Lista de nomes cadastrada no banco de dados.
+```
+5. Subir esta imagem para o docker hub
+6. Restrições:
+    1. Deverá existir um docker compose e ser possível subir todo o ambiente com docker-compose up -d
+    2. Deverá existir um volume para armazenar o banco de dados
+
+**Solução**
+
+Para executar: ``` docker compose up -d ```
